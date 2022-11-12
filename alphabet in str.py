8ddf201cc5ba880a7f5757ab4b430e4652169518
chaitanya949275 <115361706+chaitanya949275@users.noreply.github.com>
2022-11-12 11:30:12 +0530

    Add files via upload

---
+++ alphabet in str.py
@@ -0,0 +1,6 @@
+string = 'find the alphabet in the given string'
+chey = input('enter the character:')
+if chey in string:
+    print(chey,'is present in',string)
+else:
+    print(chey,'is not present',string)
